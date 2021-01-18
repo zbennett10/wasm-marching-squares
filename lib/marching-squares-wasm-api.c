769d9e4a6dabcfac83cc0aeb217104eddc0b37c9
Zachary Bennett <bennett.zachary@outlook.com>
2021-01-18 06:44:34 -0600

    removed old note

--- lib/marching-squares-wasm-api.c
+++ lib/marching-squares-wasm-api.c
@@ -3,18 +3,9 @@
 #include <emscripten/emscripten.h>
 #include "marching-squares.h"
 
-/*
- * To compile jansson C JSON library using emscripten
- * Remove jansson CMakeCache.txt
- * emcmake cmake -DCMAKE_INSTALL_PREFIX:PATH=~/dev/terra-incognita/wasm-gis-toolkit/wasm-marching-squares/include/jansson-2.13.1/emcc-lib .
- * emmake make VERBOSE=1
- * emmake make check
- * emmake make install
- * 
- * To compile with emscripten:
- * emcc marching-squares.c ./include/jansson-2.13.1/emcc-lib/lib/libjansson.a -Iinclude/jansson-2.13.1/emcc-lib/include -s EXPORTED_FUNCTIONS='["_generate_isolines_geojson", "_main"]' -s "EXTRA_EXPORTED_RUNTIME_METHODS=['ccall', 'cwrap']" -s WASM=1 -s NO_EXIT_RUNTIME=1 -s LLD_REPORT_UNDEFINED -o geojson.html 
- */
-
+/**
+ * Exported WASM API defined below
+ **/
 
 EMSCRIPTEN_KEEPALIVE char* generate_isolines_geojson_(
     int raster_data_rows,
